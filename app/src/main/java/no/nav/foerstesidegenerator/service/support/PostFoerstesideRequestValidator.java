ead759f17178a09306c311de52da53178246cabd
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-11-22 14:48:09 +0100

    MMA-1452: midlertidig fjernet tjeneste-grensesnitt (II)

--- app/src/main/java/no/nav/foerstesidegenerator/service/support/PostFoerstesideRequestValidator.java
+++ app/src/main/java/no/nav/foerstesidegenerator/service/support/PostFoerstesideRequestValidator.java
@@ -2,19 +2,19 @@ package no.nav.foerstesidegenerator.service.support;
 
 import static org.apache.logging.log4j.util.Strings.isNotBlank;
 
-import no.nav.dok.tjenester.foerstesidegenerator.PostFoerstesideRequest;
+//import no.nav.dok.tjenester.foerstesidegenerator.PostFoerstesideRequest;
 import no.nav.foerstesidegenerator.domain.code.FagomradeCode;
 import no.nav.foerstesidegenerator.exceptions.InvalidTemaException;
 
 public class PostFoerstesideRequestValidator {
 
-	public static void validate(PostFoerstesideRequest request) {
-		if (isNotBlank(request.getTema())) {
-			validateTema(request.getTema());
-		}
-
-		// flere felter
-	}
+//	public static void validate(PostFoerstesideRequest request) {
+//		if (isNotBlank(request.getTema())) {
+//			validateTema(request.getTema());
+//		}
+//
+//		// flere felter
+//	}
 
 	private static void validateTema(String tema) {
 		try {
