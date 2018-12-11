15097d57017745272c2d9ef150bbecf42f7c1229
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-11 15:23:37 +0100

    MMA-1411: Metaforceconsumer lagt til i main service

--- app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
+++ app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
@@ -2,6 +2,7 @@ package no.nav.foerstesidegenerator.service;
 
 import no.nav.dok.tjenester.foerstesidegenerator.PostFoerstesideRequest;
 import no.nav.dok.tjenester.foerstesidegenerator.PostFoerstesideResponse;
+import no.nav.foerstesidegenerator.consumer.metaforce.MetaforceConsumerService;
 import no.nav.foerstesidegenerator.service.support.PostFoerstesideRequestValidator;
 import org.springframework.stereotype.Service;
 
@@ -10,35 +11,32 @@ import javax.inject.Inject;
 @Service
 public class FoerstesideService {
 
-	// metaforce-consumer
 	// foerstesideRepository
 	private final PostFoerstesideRequestValidator postFoerstesideRequestValidator;
 	private final LoepenummerGenerator loepenummerGenerator;
+	private final MetaforceConsumerService metaforceConsumerService;
 
 	@Inject
 	public FoerstesideService(final PostFoerstesideRequestValidator postFoerstesideRequestValidator,
-							  final LoepenummerGenerator loepenummerGenerator) {
-		this.postFoerstesideRequestValidator =  postFoerstesideRequestValidator;
+							  final LoepenummerGenerator loepenummerGenerator,
+							  final MetaforceConsumerService metaforceConsumerService) {
+		this.postFoerstesideRequestValidator = postFoerstesideRequestValidator;
 		this.loepenummerGenerator = loepenummerGenerator;
+		this.metaforceConsumerService = metaforceConsumerService;
 	}
 
-	// returner en response av noe slag
 	public PostFoerstesideResponse createFoersteside(PostFoerstesideRequest request) {
-		/*
-		 * valider request []
-		 * generer løpenummer []
-		 * transformer til domeneobjekt som kan persisteres
-		 * hvis valid, lagre til db
-		 *
-		 * kall metaforce
-		 *
-		 */
 
-		// valider
+		// valider request
 		postFoerstesideRequestValidator.validate(request);
 
-		// generer loepenummer
-		String loepenummer = loepenummerGenerator.generateLoepenummer();
+		// generer løpenummer
+		loepenummerGenerator.generateLoepenummer();
+
+		// Transformer til domeneobjekt. Persister
+
+		// Kall metaforce:
+		metaforceConsumerService.createDocument(null);
 
 		return null;
 	}
@@ -48,11 +46,11 @@ public class FoerstesideService {
 		return null;
 	}
 
-	private String generateStrekkode(String loepenummer) {
-		// String kontrollsiffer1 = loepenummer mod 10
-		// String kontrollsiffer2 = (loepenummer + kontrollsiffer1 + postnummer) mod 43 ???
-		// String strekkode = * + loepenummer + kontrollsiffer1 + postnummer + kontrollsiffer2 + *
-		// totalt 16 tegn.
-		return "";
-	}
+//	private String generateStrekkode(String loepenummer) {
+//		// String kontrollsiffer1 = loepenummer mod 10
+//		// String kontrollsiffer2 = (loepenummer + kontrollsiffer1 + postnummer) mod 43 ???
+//		// String strekkode = * + loepenummer + kontrollsiffer1 + postnummer + kontrollsiffer2 + *
+//		// totalt 16 tegn.
+//		return "";
+//	}
 }
