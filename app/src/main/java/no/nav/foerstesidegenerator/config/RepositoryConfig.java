ed3202278667b85a71ea62a57076bbd5daf5c97b
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-12 08:13:32 +0100

    MMA-1453: Endret til oracle ucp

--- app/src/main/java/no/nav/foerstesidegenerator/config/RepositoryConfig.java
+++ app/src/main/java/no/nav/foerstesidegenerator/config/RepositoryConfig.java
@@ -1,8 +1,8 @@
 package no.nav.foerstesidegenerator.config;
 
 import no.nav.foerstesidegenerator.repository.FoerstesideRepository;
-import oracle.jdbc.pool.OracleDataSource;
-import org.springframework.beans.factory.annotation.Value;
+import oracle.ucp.jdbc.PoolDataSource;
+import oracle.ucp.jdbc.PoolDataSourceFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -28,46 +28,23 @@ public class RepositoryConfig {
 
 	@Bean
 	@Primary
-	DataSource dataSource(final DataSourceProperties dataSourceProperties,
-						  @Value("${dokmotds_onshosts:#{null}}") final String onsHosts) throws SQLException {
-		OracleDataSource dataSource = new OracleDataSource();
-		dataSource.setURL(dataSourceProperties.getUrl());
-		dataSource.setUser(dataSourceProperties.getUsername());
-		dataSource.setPassword(dataSourceProperties.getPassword());
-
-//		PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
-//		poolDataSource.setURL(dataSourceProperties.getUrl());
-//		poolDataSource.setUser(dataSourceProperties.getUsername());
-//		poolDataSource.setPassword(dataSourceProperties.getPassword());
-//		poolDataSource.setConnectionFactoryClassName(dataSourceProperties.getDriverClassName());
-		if(isOracleConnectionUrlFailover(dataSourceProperties.getUrl())) {
-			if(onsHosts != null) {
-				dataSource.setONSConfiguration("nodes=" + onsHosts);
-//				poolDataSource.setONSConfiguration("nodes=" + onsHosts);
-			}
-			dataSource.setFastConnectionFailoverEnabled(true);
-//			poolDataSource.setFastConnectionFailoverEnabled(true);
-		}
+	DataSource dataSource(final DataSourceProperties dataSourceProperties) throws SQLException {
+		PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
+		poolDataSource.setURL(dataSourceProperties.getUrl());
+		poolDataSource.setUser(dataSourceProperties.getUsername());
+		poolDataSource.setPassword(dataSourceProperties.getPassword());
+		poolDataSource.setConnectionFactoryClassName(dataSourceProperties.getDriverClassName());
 //
 		Properties connProperties = new Properties();
 		connProperties.setProperty("oracle.net.CONNECT_TIMEOUT", "3000");
 		connProperties.setProperty("oracle.jdbc.thinForceDNSLoadBalancing", "true");
 //		// Optimizing UCP behaviour https://docs.oracle.com/database/121/JJUCP/optimize.htm#JJUCP8143
-//		poolDataSource.setInitialPoolSize(5);
-//		poolDataSource.setMinPoolSize(2);
-//		poolDataSource.setMaxPoolSize(20);
-//		poolDataSource.setMaxConnectionReuseTime(300); // 5min
-//		poolDataSource.setMaxConnectionReuseCount(100);
-//		poolDataSource.setConnectionProperties(connProperties);
-//		return poolDataSource;
-
-		dataSource.setConnectionProperties(connProperties);
-		return dataSource;
-	}
-
-	private boolean isOracleConnectionUrlFailover(final String url) {
-		return url.toLowerCase().contains("failover");
+		poolDataSource.setInitialPoolSize(5);
+		poolDataSource.setMinPoolSize(2);
+		poolDataSource.setMaxPoolSize(20);
+		poolDataSource.setMaxConnectionReuseTime(300); // 5min
+		poolDataSource.setMaxConnectionReuseCount(100);
+		poolDataSource.setConnectionProperties(connProperties);
+		return poolDataSource;
 	}
 }
-
-
