3c9e3457ae36652c6c5c4d67a1acba170d33bdc1
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-13 14:11:21 +0100

    MMA-1452: required felter vises i swagger-grensesnitt

--- app/src/main/java/no/nav/foerstesidegenerator/config/swagger/SwaggerConfig.java
+++ app/src/main/java/no/nav/foerstesidegenerator/config/swagger/SwaggerConfig.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpHeaders;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
@@ -29,9 +31,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 )
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
-	@Value("${APP_VERSION:0.0.0}")
+	@Value("${application.version:0.0.0}")
 	private String version;
 
 	@Bean
