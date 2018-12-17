6f66b3966f96e76502daac75c5896a2ea0b53b01
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-17 14:07:59 +0100

    MMA-1453: Konstante verdi for metadata-objekter

---
+++ app/src/main/java/no/nav/foerstesidegenerator/domain/code/MetadataConstants.java
@@ -0,0 +1,30 @@
+package no.nav.foerstesidegenerator.domain.code;
+
+public class MetadataConstants {
+
+	private MetadataConstants() {
+		//no-op
+	}
+
+	public static final String ADRESSELINJE_1 = "adresselinje1";
+	public static final String ADRESSELINJE_2 = "adresselinje2";
+	public static final String ADRESSELINJE_3 = "adresselinje3";
+	public static final String POSTNUMMER = "postnummer";
+	public static final String POSTSTED = "poststed";
+	public static final String NETS_POSTBOKS = "netsPostboks";
+	public static final String AVSENDER_ID = "avsenderId";
+	public static final String AVSENDER_NAVN = "avsenderNavn";
+	public static final String BRUKER_ID = "brukerId";
+	public static final String BRUKER_TYPE = "brukerType";
+	public static final String UKJENT_BRUKER_PERSONINFO = "ukjentBrukerPersoninfo";
+	public static final String TEMA = "tema";
+	public static final String ARKIVTITTEL = "arkivtittel";
+	public static final String BREVKODE = "brevkode";
+	public static final String OVERSKRIFTSTITTEL = "overskriftstittel";
+	public static final String SPRAAKKODE = "spraakkode";
+	public static final String FOERSTESIDETYPE = "foerstesidetype";
+	public static final String VEDLEGG_LISTE = "vedleggListe";
+	public static final String ENHETSNUMMER = "enhetsnummer";
+	public static final String SAKSYSTEM = "saksystem";
+	public static final String SAKSREFERANSE = "saksreferanse";
+}
