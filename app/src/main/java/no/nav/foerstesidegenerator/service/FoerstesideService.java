2ea9a3447873e64dea6ea8bf50b40e36b717ddec
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-18 09:57:35 +0100

    MMA-1453: lagt til mapping og repo i service

--- app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
+++ app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
@@ -1,7 +1,11 @@
 package no.nav.foerstesidegenerator.service;
 
+import no.nav.dok.foerstesidegenerator.api.v1.GetFoerstesideResponse;
 import no.nav.dok.foerstesidegenerator.api.v1.PostFoerstesideRequest;
 import no.nav.dok.foerstesidegenerator.api.v1.PostFoerstesideResponse;
+import no.nav.foerstesidegenerator.domain.Foersteside;
+import no.nav.foerstesidegenerator.domain.FoerstesideMapper;
+import no.nav.foerstesidegenerator.repository.FoerstesideRepository;
 import no.nav.foerstesidegenerator.service.support.PostFoerstesideRequestValidator;
 import org.springframework.stereotype.Service;
 
@@ -11,48 +15,70 @@ import javax.inject.Inject;
 public class FoerstesideService {
 
 	// metaforce-consumer
-	// foerstesideRepository
 	private final PostFoerstesideRequestValidator postFoerstesideRequestValidator;
 	private final LoepenummerGenerator loepenummerGenerator;
+	private final FoerstesideMapper foerstesideMapper;
+	private final FoerstesideRepository foerstesideRepository;
+
+	static final String ALPHABET_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%";
 
 	@Inject
 	public FoerstesideService(final PostFoerstesideRequestValidator postFoerstesideRequestValidator,
-							  final LoepenummerGenerator loepenummerGenerator) {
+							  final LoepenummerGenerator loepenummerGenerator,
+							  final FoerstesideMapper foerstesideMapper,
+							  final FoerstesideRepository foerstesideRepository) {
 		this.postFoerstesideRequestValidator =  postFoerstesideRequestValidator;
 		this.loepenummerGenerator = loepenummerGenerator;
+		this.foerstesideMapper = foerstesideMapper;
+		this.foerstesideRepository = foerstesideRepository;
 	}
 
 	// returner en response av noe slag
 	public PostFoerstesideResponse createFoersteside(PostFoerstesideRequest request) {
-		/*
-		 * valider request []
-		 * generer løpenummer []
-		 * transformer til domeneobjekt som kan persisteres
-		 * hvis valid, lagre til db
-		 *
-		 * kall metaforce
-		 *
-		 */
-
-		// valider
+		// valider request
 		postFoerstesideRequestValidator.validate(request);
 
 		// generer loepenummer. stringify - String.format("%09d", loepenummer)
 		int loepenummer = loepenummerGenerator.generateLoepenummer();
 
-		return null;
+		// map til domeneobjekt
+		Foersteside foersteside = foerstesideMapper.map(request);
+		foersteside.setLoepenummer(String.format("%09d", loepenummer));
+
+		// persister til db
+		foerstesideRepository.save(foersteside);
+
+		// kall metaforce:
+		// byte[] document = metaforceService.createDocument()
+
+		return new PostFoerstesideResponse()
+				.withFoersteside(null);
 	}
 
-	public Object getFoersteside(String key) {
-		// todo: implement
-		return null;
+	public GetFoerstesideResponse getFoersteside(String loepenummer) {
+		/* todo
+		 * kall tjenesten med løpenummer (+ kontrollsiffer)
+		 *
+		 * hent ut fra db.
+		 * Sett uthentet=true, og uthentetTidspunkt.
+		 *
+		 * returner response med metadata.
+		 */
+		return new GetFoerstesideResponse();
 	}
 
-	private String generateStrekkode(String loepenummer) {
-		// String kontrollsiffer1 = loepenummer mod 10
-		// String kontrollsiffer2 = (loepenummer + kontrollsiffer1 + postnummer) mod 43 ???
-		// String strekkode = * + loepenummer + kontrollsiffer1 + postnummer + kontrollsiffer2 + *
-		// totalt 16 tegn.
-		return "";
+	private String generateStrekkode(int loepenummer, String postboks) {
+		int c1 = loepenummer % 10;
+		String loepenummerString = String.format("%09d", loepenummer);
+
+		String res = loepenummerString + c1 + postboks;
+
+		int total = 0;
+		for (int i = 0; i < res.length(); i++) {
+			total += ALPHABET_STRING.indexOf(res.charAt(i));
+		}
+		char c2 = ALPHABET_STRING.charAt(total % 43);
+
+		return "*" + res + c2 + "*";
 	}
 }
