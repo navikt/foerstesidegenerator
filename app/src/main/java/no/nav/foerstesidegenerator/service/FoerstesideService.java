ab2d1884f0faaeca3df38e6d1ee67e3c49c876f3
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2018-12-21 13:00:12 +0100

    MMA-1452: Lagt til lombok og logging

--- app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
+++ app/src/main/java/no/nav/foerstesidegenerator/service/FoerstesideService.java
@@ -2,11 +2,13 @@ package no.nav.foerstesidegenerator.service;
 
 import static java.lang.Integer.parseInt;
 
+import lombok.extern.slf4j.Slf4j;
 import no.nav.dok.foerstesidegenerator.api.v1.GetFoerstesideResponse;
 import no.nav.dok.foerstesidegenerator.api.v1.PostFoerstesideRequest;
 import no.nav.dok.foerstesidegenerator.api.v1.PostFoerstesideResponse;
 import no.nav.foerstesidegenerator.domain.Foersteside;
 import no.nav.foerstesidegenerator.domain.FoerstesideMapper;
+import no.nav.foerstesidegenerator.exception.FoerstesideNotFoundException;
 import no.nav.foerstesidegenerator.exception.UgyldigLoepenummerException;
 import no.nav.foerstesidegenerator.repository.FoerstesideRepository;
 import no.nav.foerstesidegenerator.service.support.GetFoerstesideResponseMapper;
@@ -15,11 +17,12 @@ import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.time.LocalDateTime;
-import java.util.Optional;
 
+@Slf4j
 @Service
 public class FoerstesideService {
 
+	private static final String ALPHABET_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%";
 	// metaforce-consumer
 	private final PostFoerstesideRequestValidator postFoerstesideRequestValidator;
 	private final LoepenummerGenerator loepenummerGenerator;
@@ -27,15 +30,13 @@ public class FoerstesideService {
 	private final FoerstesideRepository foerstesideRepository;
 	private final GetFoerstesideResponseMapper getFoerstesideResponseMapper;
 
-	private static final String ALPHABET_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-. $/+%";
-
 	@Inject
 	public FoerstesideService(final PostFoerstesideRequestValidator postFoerstesideRequestValidator,
 							  final LoepenummerGenerator loepenummerGenerator,
 							  final FoerstesideMapper foerstesideMapper,
 							  final FoerstesideRepository foerstesideRepository,
 							  final GetFoerstesideResponseMapper getFoerstesideResponseMapper) {
-		this.postFoerstesideRequestValidator =  postFoerstesideRequestValidator;
+		this.postFoerstesideRequestValidator = postFoerstesideRequestValidator;
 		this.loepenummerGenerator = loepenummerGenerator;
 		this.foerstesideMapper = foerstesideMapper;
 		this.foerstesideRepository = foerstesideRepository;
@@ -55,9 +56,14 @@ public class FoerstesideService {
 
 		// persister til db
 		foerstesideRepository.save(foersteside);
+		log.info("Har validert request og generert loepenummer for ny foersteside");
+
+		// kall dokkat:
+		// log.info(har hentet metadata fra dokkat)
 
 		// kall metaforce:
 		// byte[] document = metaforceService.createDocument()
+		// log.info(har generert ny foersteside-pdf i metaforce)
 
 		return new PostFoerstesideResponse()
 				.withFoersteside(null);
@@ -65,16 +71,14 @@ public class FoerstesideService {
 
 	public GetFoerstesideResponse getFoersteside(String loepenummer) {
 		validerLoepenummer(loepenummer);
+		log.info("Loepenummer validert ok");
 
-		Optional<Foersteside> foersteside = foerstesideRepository.findByLoepenummer(loepenummer.substring(0, 9));
-		if (foersteside.isPresent()) {
-			Foersteside domain = foersteside.get();
-			domain.setUthentet(true);
-			domain.setDatoUthentet(LocalDateTime.now());
-			return getFoerstesideResponseMapper.map(domain);
-		} else {
-			return null;
-		}
+		Foersteside domain = foerstesideRepository.findByLoepenummer(loepenummer.substring(0, 9))
+				.orElseThrow(() -> new FoerstesideNotFoundException(String.format("Kan ikke finne foersteside med loepenummer=%s", loepenummer)));
+		domain.setUthentet(true);
+		domain.setDatoUthentet(LocalDateTime.now());
+		return getFoerstesideResponseMapper.map(domain);
+
 	}
 
 	private void validerLoepenummer(String loepenummer) {
