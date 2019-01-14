a2baf942790c62966994fd4358fe806f5406ee4e
Tveter, Martin <Martin.Skjelsvik.Tveter@nav.no>
2019-01-14 10:01:06 +0100

    slettet ubrukte klasser

--- app/src/main/java/no/nav/foerstesidegenerator/consumer/metaforce/update/Metaforce.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foerstesidegenerator.consumer.metaforce.update;
-
-import no.nav.foerstesidegenerator.consumer.metaforce.to.CreateDocumentRequestTo;
-import no.nav.foerstesidegenerator.consumer.metaforce.to.CreateDocumentResponseTo;
-
-public interface Metaforce {
-	CreateDocumentResponseTo createDocument(CreateDocumentRequestTo createDocumentRequestTo);
-}
