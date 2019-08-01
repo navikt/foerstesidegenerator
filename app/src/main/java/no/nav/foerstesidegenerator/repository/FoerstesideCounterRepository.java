65d6eaff3ba866e8c0597fade4fc6e03c11f313f
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-08-01 21:45:37 +0200

    MMA-91: Trying JPQL

--- app/src/main/java/no/nav/foerstesidegenerator/repository/FoerstesideCounterRepository.java
+++ app/src/main/java/no/nav/foerstesidegenerator/repository/FoerstesideCounterRepository.java
@@ -2,16 +2,21 @@ package no.nav.foerstesidegenerator.repository;
 
 import no.nav.foerstesidegenerator.domain.FoerstesideCounter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.LockModeType;
+
 @Repository
 public interface FoerstesideCounterRepository extends JpaRepository<FoerstesideCounter, Long> {
-    @Query(value =
-            "select * " +
-                    "FROM " + FoerstesideCounter.TABLE_NAME + " " +
-                    "WHERE DATO = to_char(sysdate, 'YYYYMMDD')", nativeQuery = true)
+//    @Query(value =
+//            "select * " +
+//                    "FROM " + FoerstesideCounter.TABLE_NAME + " " +
+//                    "WHERE DATO = to_char(sysdate, 'YYYYMMDD')", nativeQuery = true)
     @Transactional
+    @Query("SELECT fc FROM FoerstesideCounter fc WHERE fc.date = '20190801'")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     FoerstesideCounter getCounterForToday();
 }
