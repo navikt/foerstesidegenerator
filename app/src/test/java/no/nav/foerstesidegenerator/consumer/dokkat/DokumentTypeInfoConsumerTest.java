5324b676a2112b57e0abba4b7b242d7d1158519e
Kristian Laskemoen <kristian.laskemoen@nav.no>
2022-10-25 10:41:05 +0200

    MMA-6284: Tatt bort ubrukte imports

--- app/src/test/java/no/nav/foerstesidegenerator/consumer/dokkat/DokumentTypeInfoConsumerTest.java
+++ app/src/test/java/no/nav/foerstesidegenerator/consumer/dokkat/DokumentTypeInfoConsumerTest.java
@@ -1,27 +1,21 @@
 package no.nav.foerstesidegenerator.consumer.dokkat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.reset;
-import static org.mockito.Mockito.when;
-
 import no.nav.dokkat.api.tkat020.v4.DokumentProduksjonsInfoToV4;
 import no.nav.dokkat.api.tkat020.v4.DokumentTypeInfoToV4;
 import no.nav.foerstesidegenerator.consumer.dokkat.to.DokumentTypeInfoTo;
-import no.nav.foerstesidegenerator.exception.DokkatConsumerFunctionalException;
-import no.nav.foerstesidegenerator.exception.FoerstesideGeneratorTechnicalException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DokumentTypeInfoConsumerTest {
