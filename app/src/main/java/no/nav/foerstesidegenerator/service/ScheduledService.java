41c38d35382e76f2e9ff36ced15088b14bb40ed4
Joakim Borgersen <joakim.borgersen@nav.no>
2025-01-06 06:51:30 +0100

    MMA-7746 add leader election for den skedulerte maskeringsjobben

--- app/src/main/java/no/nav/foerstesidegenerator/service/ScheduledService.java
+++ app/src/main/java/no/nav/foerstesidegenerator/service/ScheduledService.java
@@ -1,6 +1,7 @@
 package no.nav.foerstesidegenerator.service;
 
 import lombok.extern.slf4j.Slf4j;
+import no.nav.foerstesidegenerator.consumer.lederelection.LeaderElectionConsumer;
 import no.nav.foerstesidegenerator.repository.FoerstesideRepository;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,20 +17,27 @@ import static no.nav.foerstesidegenerator.domain.code.MetadataConstants.UKJENT_B
 public class ScheduledService {
 
 	private final FoerstesideRepository foerstesideRepository;
+	private final LeaderElectionConsumer leaderElectionConsumer;
 
-	public ScheduledService(final FoerstesideRepository foerstesideRepository) {
+	public ScheduledService(final FoerstesideRepository foerstesideRepository, LeaderElectionConsumer leaderElectionConsumer) {
 		this.foerstesideRepository = foerstesideRepository;
+		this.leaderElectionConsumer = leaderElectionConsumer;
 	}
 
 	@Transactional
 	@Scheduled(cron = "${maskering.fnr.rate}")
 	public void execute() {
-		log.info("Starter automatisk jobb for maskering av førstesidemetadata");
+		log.info("Sjekker om denne podden er leader for maskering av førstedata");
+		if (leaderElectionConsumer.isLeader()) {
+			log.info("Starter automatisk jobb for maskering av førstesidemetadata");
 
-		masker(BRUKER_ID);
-		masker(UKJENT_BRUKER_PERSONINFO);
+			masker(BRUKER_ID);
+			masker(UKJENT_BRUKER_PERSONINFO);
 
-		log.info("Avslutter automatisk jobb for maskering av førstesidemetadata");
+			log.info("Avslutter automatisk jobb for maskering av førstesidemetadata");
+		} else {
+			log.info("Denne podden er ikke leader. Kjører ikke mskering av førstedata på denne podden.");
+		}
 	}
 
 	private void masker(String metadataKey) {
