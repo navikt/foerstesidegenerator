026939781a18b63f80e8f8efaae180565210d446
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2026-03-16 16:54:44 +0100

    Vasker brukerId som ikke validerer før det logges

--- app/src/test/java/no/nav/foerstesidegenerator/service/support/PostFoerstesideRequestValidatorTest.java
+++ app/src/test/java/no/nav/foerstesidegenerator/service/support/PostFoerstesideRequestValidatorTest.java
@@ -70,6 +70,23 @@ class PostFoerstesideRequestValidatorTest {
 		assertDoesNotThrow(() -> validator.validate(request, defaultHeaders));
 	}
 
+	@ParameterizedTest
+	@MethodSource
+	void shouldThrowSanitizedExceptionWhenBrukerIdInvalid(String brukerId, String expectedBrukerIdMessage) {
+		PostFoerstesideRequest request = createRequestWithBrukerId(brukerId);
+
+		BrukerIdIkkeValidException brukerIdIkkeValidException = assertThrows(BrukerIdIkkeValidException.class, () -> validator.validate(request, defaultHeaders));
+		assertThat(brukerIdIkkeValidException.getMessage()).contains("Validering av ident feilet. brukerId=" + expectedBrukerIdMessage + ", brukerType=PERSON. Kunne ikke opprette førsteside.");
+	}
+
+	private static Stream<Arguments> shouldThrowSanitizedExceptionWhenBrukerIdInvalid() {
+		return Stream.of(
+				Arguments.of("111111111111", "11111111111"),
+				Arguments.of("${jndi:ldap://example.com/file}", "__jndi_ldap")
+		);
+	}
+
+
 	@Test
 	void shouldThrowExceptionIfSpraakkodeIsNull() {
 		PostFoerstesideRequest request = PostFoerstesideRequest.builder()
